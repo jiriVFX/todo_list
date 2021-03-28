79abf7cd8d16e8f9ea0486963d33a6cb23c3435c
jirin <jirinovy@outlook.com>
2021-03-28 22:49:07 +0100

    Initial commit

---
+++ forms.py
@@ -0,0 +1,12 @@
+from flask_wtf import FlaskForm
+from wtforms import StringField, SubmitField, BooleanField, DateTimeField
+from wtforms.validators import DataRequired
+
+
+# WTForm
+class AddTaskForm(FlaskForm):
+    task_name = StringField("Task name", validators=[DataRequired()])
+    description = StringField("Description", validators=[DataRequired()])
+    finish_by = DateTimeField("Finish by", validators=[DataRequired()])
+    finished = BooleanField("Finished")
+    submit = SubmitField("Add Task")
---
+++ main.py
@@ -0,0 +1,60 @@
+from flask import Flask, jsonify, render_template, request, flash, redirect, url_for, session
+from flask_sqlalchemy import SQLAlchemy
+from flask_bootstrap import Bootstrap
+import os
+from datetime import datetime
+from forms import AddTaskForm
+
+app = Flask(__name__)
+Bootstrap(app)
+app.config["SECRET_KEY"] = os.environ.get("SECRET_KEY")
+# .env file should contain your API_KEY, e.g. API_KEY=YourOwnApiKey
+API_KEY = os.environ.get("API_KEY")
+# Connect to the Database
+app.config["SQLALCHEMY_DATABASE_URI"] = "sqlite:///tasks.db"
+app.config["SQLALCHEMY_TRACK_MODIFICATIONS"] = False
+db = SQLAlchemy(app)
+
+
+# To-do list Table Configuration
+class ToDo(db.Model):
+    id = db.Column(db.Integer, primary_key=True)
+    task_name = db.Column(db.String(100), unique=True, nullable=False)
+    description = db.Column(db.String(250), nullable=True)
+    finish_by = db.Column(db.DateTime(timezone=True), nullable=True)
+    finished = db.Column(db.Boolean, nullable=False)
+
+
+@app.route("/")
+def home():
+    page = request.args.get("page", default=1, type=int)
+    todo = db.session.query(ToDo).paginate(per_page=10)
+    #user_agent = request.headers.get("User-Agent")
+    return render_template("todo.html", tasks=todo)
+
+
+@app.route("/add")
+def add():
+    page = request.args.get("page", default=1, type=int)
+    return render_template("todo.html")
+
+@app.route("/edit")
+def edit():
+    page = request.args.get("page", default=1, type=int)
+    return render_template("todo.html")
+
+
+@app.route("/remove")
+def remove():
+    page = request.args.get("page", default=1, type=int)
+    return render_template("todo.html")
+
+
+if __name__ == '__main__':
+    #db.create_all()
+
+    # todo_list = ToDo(task_name="Third record", description="Finish the third record", finish_by=datetime.now(), finished=False)
+    # db.session.add(todo_list)
+    # db.session.commit()
+
+    app.run(debug=True)
